aabf4b59615d2af69f700318afbceb158cff325a
MuhammadAhmed-Developer <119342876+MuhammadAhmed-Developer@users.noreply.github.com>
2024-07-06 15:03:06 +0500

    add ads

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,32 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Nextjs Project For Ads</h1>
+      <ins
+        className="adsbygoogle"
+        style={{ display: "block" }}
+        data-ad-client="ca-pub-4293091400623161"
+        data-ad-slot="5816307885"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      ></ins>
+      <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4293091400623161"
+        crossOrigin="anonymous"
+      ></script>
+      <ins
+        className="adsbygoogle"
+        style={{
+          display: "block",
+          textAlign: "center",
+        }}
+        data-ad-layout="in-article"
+        data-ad-format="fluid"
+        data-ad-client="ca-pub-4293091400623161"
+        data-ad-slot="5979959419"
+      ></ins>
+      <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
     </main>
   );
 }
