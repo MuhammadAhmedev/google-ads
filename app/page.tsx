b99759b75c61532069ee47e16c6ff4291d4687c5
MuhammadAhmed-Developer <119342876+MuhammadAhmed-Developer@users.noreply.github.com>
2024-07-06 15:17:44 +0500

    testing ads

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,17 @@
-// Home.tsx
+"use client";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      if (!window.adsbygoogle) {
+        window.adsbygoogle = [];
+      }
+      window.adsbygoogle.push({});
+      window.adsbygoogle.push({});
+    }
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Nextjs Project For Ads</h1>
@@ -12,24 +23,14 @@ export default function Home() {
         data-ad-format="auto"
         data-full-width-responsive="true"
       ></ins>
-      <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
-      <script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4293091400623161"
-        crossOrigin="anonymous"
-      ></script>
       <ins
         className="adsbygoogle"
-        style={{
-          display: "block",
-          textAlign: "center",
-        }}
+        style={{ display: "block", textAlign: "center" }}
         data-ad-layout="in-article"
         data-ad-format="fluid"
         data-ad-client="ca-pub-4293091400623161"
         data-ad-slot="5979959419"
       ></ins>
-      <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
     </main>
   );
 }
